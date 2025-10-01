0838f02c79f5eeb5fca42cd40d63940bc271204e
Ivan Yadzvinsky <ivan.yadzvinsky@gmail.com>
2025-10-01 14:58:49 -0400

    changes in fugitive config
    added --decorate parameter to git log key binding in order to show HEAD
    and branch references

--- nvim/lua/tdk/plugins/fugitive.lua
+++ nvim/lua/tdk/plugins/fugitive.lua
@@ -2,5 +2,5 @@ vim.pack.add({ 'https://github.com/tpope/vim-fugitive' })
 
 vim.keymap.set('n', '<c-g>', '<cmd>vert Git<cr>', { desc = 'Open Git Status window in vertical split' })
 vim.keymap.set('n', '<leader>gb', '<cmd>Git blame<cr>', { desc = 'Open Git Blame window' })
-vim.keymap.set('n', '<leader>gl', '<cmd>vert Git log<cr>', { desc = 'Open Git Log window' })
+vim.keymap.set('n', '<leader>gl', '<cmd>vert Git log --decorate<cr>', { desc = 'Open Git Log window' })
 
